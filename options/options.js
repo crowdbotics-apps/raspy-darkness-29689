159937d7c6e6aac76ac70bcbc4eb0466c2db2753
Crowdbotics <team@crowdbotics.com>
2021-08-10 13:39:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "raspy_darkness_29689Identifier",
+  url: "https://raspy_darkness_29689Identifier.botics.co",
+  api: "https://raspy_darkness_29689Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
